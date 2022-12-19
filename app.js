75ac62d389a8c83b86f9b6d7c9006c1bded0c34d
Vladimir Nedorezov <nedvov@yandex.ru>
2022-12-19 23:03:27 +0300

    feat: make app safety by env, helmet and limitter

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
@@ -9,6 +12,18 @@ const { handleCors } = require('./middlewares/handleCors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
+const { MONGO_URL } = process.env;
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+
+mongoose.set('strictQuery', true);
+
+app.use(helmet());
+
+app.use(limiter);
 
 app.use(bodyParser.json());
 app.use(cookieParser());
@@ -19,10 +34,13 @@ app.use(requestLogger);
 app.use(router);
 app.use(errorLogger);
 
+mongoose.connect(MONGO_URL, {})
+  .catch((err) => {
+    console.log(err.message);
+  });
+
 app.use(errors({ message: 'Ошибка. Переданы некорректные данные' }));
 
 app.use(handleErrors);
-
-mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {});
 
 module.exports = app;
